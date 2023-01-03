a62068d4b1339b56285e7b9d65f412273a5b4e6a
Gowtham <78350358+Gowtham011@users.noreply.github.com>
2023-01-03 15:44:47 +0530

    Update Solution_three.java

--- Solution_three.java
+++ Solution_three.java
@@ -4,20 +4,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths; 
+import java.util.stream.Stream;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.*;
+  
 public class Solution_three {
+	 private final static Logger LOGGER = 
+             Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	public static void moveFile(String src, String dest ) {
-       Path result = null;
-       try {
-          result = Files.move(Paths.get(src), Paths.get(dest));
-       } catch (IOException e) {
-          System.out.println("Exception while moving file: " + e.getMessage());
-       }
-       if(result != null) {
-          System.out.println("File moved successfully.");
-       }else{
-          System.out.println("File movement failed.");
-       }
-    }
+        Path result = null;
+        try {
+           result = Files.move(Paths.get(src), Paths.get(dest));
+        } catch (IOException e) {
+           System.out.println("Exception while moving file: " + e.getMessage());
+        }
+        if(result != null) {
+        	LOGGER.log(Level.INFO, "File successfully moved");
+        }else{
+        	LOGGER.log(Level.INFO, "File movement process failed");
+        }
+     }
 
 	public static void main(String[] args) {
 	 
